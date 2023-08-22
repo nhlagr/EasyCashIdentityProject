a4a9c6cb7ab4600cf45ed46790360cb7c1875342
nhlagr <102624700+nhlagr@users.noreply.github.com>
2023-08-22 16:49:33 +0300

    ders4

--- EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,14 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Concrete
 {
-    internal class Context
+    public class Context:IdentityDbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-4O9M46S\\SQLEXPRESS; initial catalog= EasyCashDb;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccount { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcess { get; set; }
     }
 }
